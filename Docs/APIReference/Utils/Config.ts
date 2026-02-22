3e48478b4f5504b5985fd2ff8c26e78a1209b08d
Nawaz Dhandala <hello@nawazdhandala.com>
2026-02-22 13:40:51 +0000

    Add sidenav partial for API reference documentation

---
+++ Docs/APIReference/Utils/Config.ts
@@ -0,0 +1,3 @@
+export const ViewsPath: string = "/usr/src/app/APIReference/views";
+export const StaticPath: string = "/usr/src/app/APIReference/Static";
+export const CodeExamplesPath: string = "/usr/src/app/APIReference/CodeExamples";
