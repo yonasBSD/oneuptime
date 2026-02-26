a6642ac1b7dfab67f863ec949b332b49016e5920
Nawaz Dhandala <hello@nawazdhandala.com>
2026-02-26 13:20:33 +0000

    feat: Add migration for firstSeenInRelease, lastSeenInRelease, and environment columns in TelemetryException

--- Common/Server/Infrastructure/Postgres/SchemaMigrations/Index.ts
+++ Common/Server/Infrastructure/Postgres/SchemaMigrations/Index.ts
@@ -260,6 +260,7 @@ import { MigrationName1770833704656 } from "./1770833704656-MigrationName";
 import { MigrationName1770834237090 } from "./1770834237090-MigrationName";
 import { MigrationName1770834237091 } from "./1770834237091-MigrationName";
 import { MigrationName1772110755000 } from "./1772110755000-MigrationName";
+import { MigrationName1772111896988 } from "./1772111896988-MigrationName";
 
 export default [
   InitialMigration,
@@ -523,5 +524,5 @@ export default [
   MigrationName1770833704656,
   MigrationName1770834237090,
   MigrationName1770834237091,
-  MigrationName1772110755000,
+  MigrationName1772111896988
 ];
